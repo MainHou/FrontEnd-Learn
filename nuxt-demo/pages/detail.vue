4cb73ff7d31eb606598b3e75e34fd97ef2aaa80e
MainHou <mainhou@qq.com>
2022-06-05 15:44:05 +0800

    ✨ feat: nuxt param route

---
+++ nuxt-demo/pages/detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">Detail Page</div>
+</template>
+
+<script lang="ts">
+import {reactive, toRefs, defineComponent} from 'vue';
+
+export default defineComponent({
+  setup() {
+    const data = reactive({});
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped></style>
---
+++ nuxt-demo/pages/user-[group]/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">user page</div>
+  <p>group:{{ $route.params.group }}</p>
+  <p>id:{{ $route.params.id }}</p>
+</template>
+
+<script lang="ts">
+import {reactive, toRefs, defineComponent} from 'vue';
+
+export default defineComponent({
+  setup() {
+    const data = reactive({});
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped></style>
