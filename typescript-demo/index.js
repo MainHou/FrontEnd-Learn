ffd4e97a3da3113f0bf84f6acc7b1f0e9190dd36
MainHou <mainhou@qq.com>
2022-08-06 12:32:19 +0800

    ✨feat: add ts

--- typescript-demo/index.js
+++ typescript-demo/index.js
@@ -1,4 +1,16 @@
-var str = 'mainhou';
-var num = 1;
-var big = 10n;
+"use strict";
+let str = 'mainhou';
+let num = 1;
+let big = 10n;
 console.log(str, num, big);
+let obj1 = { a: 1, b: 2 };
+obj1.a = 2;
+let obj;
+obj = 1; // ok
+obj = 'a'; // ok
+obj = true; // ok
+obj = {}; // ok
+obj = Symbol(); //ok
+obj = 10n; //ok
+obj = null; // error
+obj = undefined; // error
--- typescript-demo/index.ts
+++ typescript-demo/index.ts
@@ -2,3 +2,14 @@ let str: string = 'mainhou';
 let num: number = 1;
 let big: bigint = 10n;
 console.log(str, num, big);
+let obj1: object = {a: 1, b: 2};
+obj1.a = 2;
+let obj: Object;
+obj = 1; // ok
+obj = 'a'; // ok
+obj = true; // ok
+obj = {}; // ok
+obj = Symbol(); //ok
+obj = 10n; //ok
+obj = null; // error
+obj = undefined; // error
