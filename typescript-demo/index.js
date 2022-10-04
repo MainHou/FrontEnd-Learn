cf2d9aa49a23775df65ede10a22942a187f57a03
houyusheng <1440248234@qq.com>
2022-10-04 19:10:28 +0800

    ✨ feat(typescript-demo): add learn typescript

--- typescript-demo/index.js
+++ typescript-demo/index.js
@@ -4,7 +4,7 @@ let num = 1;
 let big = 10n;
 console.log(str, num, big);
 let obj1 = { a: 1, b: 2 };
-obj1.a = 2;
+// obj1.a = 2; // error
 let obj;
 obj = 1; // ok
 obj = 'a'; // ok
@@ -12,5 +12,5 @@ obj = true; // ok
 obj = {}; // ok
 obj = Symbol(); //ok
 obj = 10n; //ok
-obj = null; // error
-obj = undefined; // error
+// obj = null; // error
+// obj = undefined; // error
--- typescript-demo/index.ts
+++ typescript-demo/index.ts
@@ -1,15 +1,22 @@
-let str: string = 'mainhou';
+let str: string = "mainhou";
 let num: number = 1;
 let big: bigint = 10n;
 console.log(str, num, big);
-let obj1: object = {a: 1, b: 2};
-obj1.a = 2;
+let obj1: object = { a: 1, b: 2 };
+// obj1.a = 2; // error
 let obj: Object;
 obj = 1; // ok
-obj = 'a'; // ok
+obj = "a"; // ok
 obj = true; // ok
 obj = {}; // ok
 obj = Symbol(); //ok
 obj = 10n; //ok
-obj = null; // error
-obj = undefined; // error
+// obj = null; // error
+// obj = undefined; // error
+let a = (p1: number, p2: number) => {};
+let b = (p1?: number, p2?: number) => {};
+let c = (...arg: number[]) => {};
+a = b;
+a = c;
+// b = c; //error
+// b = a; //error
