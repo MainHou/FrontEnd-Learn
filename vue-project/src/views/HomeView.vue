7cade50cba5374c38d94e02f539da2eee1e05295
YuShengHou <mainhou@qq.com>
2024-07-04 17:04:37 +0800

    ✨ feat:add vue-project

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="tsx">
+// import TheWelcome from '../components/TheWelcome.vue'
+const Foo = (props: { msg: string }) => {
+  return <div>{props.msg}</div>
+}
+</script>
+
+<template>
+  <main>
+    <Foo :msg="'hello vue'" />
+  </main>
+</template>
